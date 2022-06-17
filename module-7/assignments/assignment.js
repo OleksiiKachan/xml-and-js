12d7a3948124484fb65e7dd2ba168f78b0cdf7a4
miraphilip5 <miirapanji5@gmail.com>
2022-06-16 21:52:36 -0400

    module 7 assignment files

---
+++ module-7/assignments/assignment.js
@@ -0,0 +1,21 @@
+const data = [
+  { born: 1870, died: 1924 },
+  { born: 1893, died: 1976 },
+  { born: 1869, died: 1948 },
+  { born: 1901, died: 1989 },
+];
+
+const age = data.map(({ born, died }) => died - born);
+
+const filtered = age.filter((data) => data > 75);
+
+const oldest = filtered.reduce((acc, value) => (acc > value ? acc : value));
+
+// Refactoring to use chaining
+console.log(
+  data
+    .map(({ born, died }) => died - born)
+    .filter((data) => data > 75)
+    .reduce((acc, value) => (acc > value ? acc : value))
+);
+
