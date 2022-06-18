b1111e4b4b73df87c0443d36291a6d93cb6038f2
Mudit Khanna <muditkhanna998@gmail.com>
2022-06-17 21:23:28 -0400

    Adds module 7 assignment- Mudit Khanna

---
+++ module-7/assignments/assignment.js
@@ -0,0 +1,26 @@
+const data = [
+  { born: 1870, died: 1924 },
+  { born: 1893, died: 1976 },
+  { born: 1869, died: 1948 },
+  { born: 1901, died: 1989 },
+];
+
+// const age = data.map((item) => item.died - item.born);
+// const filtered = age.filter((item) => item > 75);
+// const oldest = filtered.reduce((acc, item) => {
+//   if (item > acc) {
+//     acc = item;
+//   }
+//   return acc;
+// });
+
+const oldest = data
+  .map((item) => item.died - item.born)
+  .filter((item) => item > 75)
+  .reduce((acc, item) => {
+    if (item > acc) {
+      acc = item;
+    }
+    return acc;
+  });
+console.log(oldest);
