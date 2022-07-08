737588a8265bb941ddf2c43ce733f3ead94c93d8
miraphilip5 <miirapanji5@gmail.com>
2022-07-08 02:03:05 -0400

    file updates

--- module-8/assignments/activity.md
+++ module-8/assignments/activity.md
@@ -1,4 +1,6 @@
 # Activity
 
-- Rewrite sync.js using Promises​
-- Attach screenshot of the output in your terminal
+- Rewrite sync.js using Promises​ - `DONE`
+- Attach screenshot of the output in your terminal 
+
+![image info](./activity-output.png)
--- module-8/sync-promise.js
+++ module-8/sync-promise.js
@@ -1,17 +1,17 @@
 const wait = () => new Promise((resolve) => setTimeout(resolve, 2000));
 
-const inc = (a) => new Promise((resolve) => resolve(a + 1));
+const inc = (a) => wait().then(() => a + 1);
 
-const sum = (a, b) => new Promise((resolve) => resolve(a + b));
+const sum = (a, b) => wait().then(() => a + b);
 
-const max = (a, b) => new Promise((resolve) => resolve(a > b ? a : b));
+const max = (a, b) => wait().then(() => (a > b ? a : b));
 
-const avg = (a, b) => new Promise((resolve) => resolve(sum(a, b) / 2));
+const avg = (a, b) => wait().then(() => sum(a, b).then((s) => s / 2));
 
 const obj = {
   name: "Marcus Aurelius",
   split(sep = " ") {
-    return new Promise((resolve) => resolve(this.name.split(sep)));
+    return wait().then(() => this.name.split(sep));
   },
 };
 
@@ -25,24 +25,27 @@ class Person {
   }
 
   split(sep = " ") {
-    return new Promise((resolve) => resolve(this.name.split(sep)));
+    return wait().then(() => this.name.split(sep));
   }
 }
 
 const person = Person.of("Marcus Aurelius");
 
-inc(5)
-  .then((data) => {
-    console.log(data);
-  })
-  .then(wait)
-  .then(sum(1, 3))
-  .then((data) => {
-    console.log(data);
-  })
-  .then(wait)
-  .then(max(8, 6))
-  .then((data) => {
-    console.log(data);
-  })
-  .then(wait);
+inc(5).then((data) => {
+  console.log("inc(5) = ", data);
+  sum(1, 3).then((data) => {
+    console.log("sum(1,3) = ", data);
+    max(8, 6).then((data) => {
+      console.log("max(8,6) = ", data);
+      avg(8, 6).then((data) => {
+        console.log("avg(8,6) = ", data);
+        obj.split().then((data) => {
+          console.log("obj.split() = ", data);
+          person.split().then((data) => {
+            console.log("person.split() = ", data);
+          });
+        });
+      });
+    });
+  });
+});
