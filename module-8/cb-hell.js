dda5cadc63353b9040c6f25af1d0a3f5d559076c
miraphilip5 <miirapanji5@gmail.com>
2022-07-10 16:38:02 -0400

    module8 files

--- module-8/cb-hell.js
+++ module-8/cb-hell.js
@@ -4,7 +4,7 @@
  * 3. Make sure the "Finish" is logged after all the data is converted
  */
 
-const timeout = (ms, callback) => {
+const timeout = async (ms) => {
   return new Promise(function (resolve) {
     setTimeout(function () {
       resolve();
@@ -16,30 +16,22 @@ const generateRandomNumber = () =>  Math.floor(Math.random() * 40);
 
 const generateData = async ()  => {
   await timeout(1000);
-  const data = Array.from({ length: 20 }, generateRandomNumber);
-  return data;
+  return Array.from({ length: 20 }, generateRandomNumber);
 }
 
 const convertToFeet = async (meters) => {
-  const feet = meters * 3.2808;
   await timeout(3500);
-  return feet;
+  logResult(meters, meters * 3.2808);
 }
 
-const processData =  (data) => {
-  data.map(function (value) {
-    return value;
-  });
-}
+const processData =  async (data, callback) => Promise.all(data.map(value  => callback(value)));
 
 const logResult = (meters, feet) => console.log(`Converted ${meters}m to ${feet}ft`);
 
 const main = async () => {
   console.log("Start");
   const data = await generateData();
-  const value = processData(data);
-  const result = await convertToFeet(value);
-  logResult(value, result);
+  await processData(data, convertToFeet);
   console.log("Finish");
 }
 
