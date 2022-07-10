ec57924be3919af36a29e760b80b123779b44bb0
Ishita2203 <97850577+Ishita2203@users.noreply.github.com>
2022-07-09 21:00:21 -0400

    assignment completed

--- module-8/cb-hell.js
+++ module-8/cb-hell.js
@@ -4,7 +4,7 @@
  * 3. Make sure the "Finish" is logged after all the data is converted
  */
 
-function timeout(ms, callback) {
+/*function timeout(ms, callback) {
   return new Promise(function (resolve) {
     setTimeout(function () {
       resolve();
@@ -53,4 +53,45 @@ function main() {
   console.log("Finish");
 }
 
-main();
+main();*/
+
+const timeout = (ms) => {
+  return new Promise(function (resolve) {
+    setTimeout(function () {
+      resolve();
+    }, ms);
+  });
+};
+
+generateRandomNumber = () => {
+  return Math.floor(Math.random() * 40);
+};
+
+generateData = async () => {
+  await timeout(1000);
+  return Array.from({ length: 20 }, generateRandomNumber);
+};
+
+convertToFeet = async (meters) => {
+  await timeout(1000);
+  return meters * 3.2808;
+};
+
+const processData = async (data, callback) => {
+  const List = data.map(async (value) => await callback(value));
+  return await Promise.all(List);
+};
+
+logResult = (meters, feet) => {
+  console.log(`CONVERTED ${meters} m TO ${feet} ft`);
+};
+
+(async () => {
+  console.log("-------------START-------------");
+  const randomDataList = await generateData();
+  const heightInFeet = await processData(randomDataList, convertToFeet);
+  for (let i = 0; i < randomDataList.length; i++) {
+    logResult(randomDataList[i], heightInFeet[i]);
+  }
+  console.log("-------------FINISH-------------");
+})();
