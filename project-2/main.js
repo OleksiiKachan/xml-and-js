0bcbac69ae425cb6c06ddfc72ee91e1ba2b2bc8e
miraphilip5 <miirapanji5@gmail.com>
2022-08-08 14:12:44 -0400

    changes

--- project-2/main.js
+++ project-2/main.js
@@ -1,19 +1,10 @@
-// const API_KEY = `5uWQFX4UpC76WU2QCiX5PoSzCyDqfdo6`;
-
 let _symbolsData = [];
 let _convertedData = 0;
 
 const getSymbols = async () => {
-  const result = await fetch(
-    // `https://api.exchangeratesapi.io/v1/symbols?access_key=${API_KEY}`,
-    // `https://api.apilayer.com/currency_data/list`,
-    // `https://data.fixer.io/api/symbols?access_key=${API_KEY}`,
-    `https://api.exchangerate.host/symbols`,
-    {
-      method: "GET",
-      // headers: { Authorization: "Bearer " + token },
-    }
-  );
+  const result = await fetch(`https://api.exchangerate.host/symbols`, {
+    method: "GET",
+  });
 
   const data = await result.json();
   return data.symbols;
@@ -21,11 +12,9 @@ const getSymbols = async () => {
 
 const convertCurrency = async ({ from, to, amount }) => {
   const result = await fetch(
-    // `https://api.exchangeratesapi.io/v1/convert?access_key=${API_KEY}&from=${from}&to=${to}&amount=${amount}`,
     `https://api.exchangerate.host/convert?from=${from}&to=${to}&amount=${amount}`,
     {
       method: "GET",
-      //   headers: { Authorization: "Bearer " + token },
     }
   );
 
