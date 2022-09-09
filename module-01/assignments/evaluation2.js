12c5ee375b04f04cdb3802757dfed2d8a1107f63
hardi09 <hardimajmundar0906@gmail.com>
2022-09-09 11:57:24 -0400

    complete activity 1

---
+++ module-01/assignments/evaluation2.js
@@ -0,0 +1,6 @@
+function send() {
+    var txt1 = document.getElementById("txt1").value;
+    var txt2 = document.getElementById("txt2").value;
+    var txt3 = document.getElementById("txt3").value;
+    spanResult.innerHTML= txt1+ ' '+txt2+ '  '+txt3;    
+}
