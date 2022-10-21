03985e423b4224910622b87d420b3716aa9f740f
itsAtom <dhruvdesai770@gmail.com>
2022-10-21 12:14:58 -0700

    module-7/activity Completed

---
+++ module-07/assignments/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+return [da.id = "id" + ": " + da.id,
+    // da.first_name = "firstName" + ": " + da.first_name,
+    // da.last_name = "lasttName" + ": " + da.last_name,
+    // da.gender = "gender" + ": " + da.gender,
+    // da.ip_address = "ipAddress" + ": " + da.ip_address,];
