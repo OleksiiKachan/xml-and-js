76c970efc4887aed593caac68d0cdfaf093517a6
hardi09 <hardimajmundar0906@gmail.com>
2022-10-21 14:37:51 -0400

    Done assignment

---
+++ module-07/assignments/assignment.js
@@ -0,0 +1,29 @@
+const data =[
+        { born: 1870, died: 1924 },
+        { born: 1893, died: 1976 },
+        { born: 1869, died: 1948 },
+        { born: 1901, died: 1989 }
+]
+const age = data.map( ({born, died}) => died-born)
+console.log(age);
+
+const filtered = age.filter(age => age > 75);
+console.log(filtered);
+
+const oldest = filtered.reduce((acc,item) => {
+    if(item > acc)
+        acc = item;
+    return acc;
+}, 0);
+console.log(oldest);
+
+
+const chaining = data
+    .map( ({born, died}) => died-born)
+    .filter(age => age > 75)
+    .reduce((acc,item) => {
+        if(item > acc)
+            acc = item;
+        return acc;
+    }, 0);
+console.log(chaining);
