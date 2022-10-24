63950bf553d61839b6ee2be372de7e4db22f1dad
Harishnns <108423120+Harishnns@users.noreply.github.com>
2022-10-24 18:47:36 -0400

    add eval

---
+++ module-07/assignments/assignment.js
@@ -0,0 +1,26 @@
+const data = [
+    { born: 1870, died: 1924 },
+    { born: 1893, died: 1976 },
+    { born: 1869, died: 1948 },
+    { born: 1901, died: 1989 },
+  ];
+console.log('\n ******************Age****************');
+  const age = data.map((d)=>{
+    return d.died - d.born;
+  });
+console.log(age);
+
+console.log('\n**************Filtered****************')
+  const filtered = age.filter((df)=>{
+    if(df>75){
+        return df;
+    }
+  });
+
+  console.log(filtered);
+
+  console.log('\n***************Oldest*****************')
+
+  const oldest = filtered.reduce((accum,data)=>accum || (accum.died - accum.born)<(accum.died-accum.born)?data:accum);
+
+  console.log(oldest);
