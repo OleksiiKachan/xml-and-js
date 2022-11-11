49e7c420e376081a4bb1594bf1b5ba17c8bb5028
Vasyl Marusiak <72507878+CartmanUA7@users.noreply.github.com>
2022-11-11 12:53:17 -0500

    activity from module-8

--- module-08/async.js
+++ module-08/async.js
@@ -6,35 +6,22 @@ function timeout(ms) {
   });
 }
 
-async function inc(a) {
-  await timeout(2000);
-  return a + 1;
-}
+const inc = (a) => new Promise((resolve) => timeout(2000).then(() => resolve(a+1)));
 
-const sum = async function (a, b) {
-  await timeout(2000);
-  return a + b;
-};
+const sum = (a, b) => new Promise((resolve) => timeout(2000).then(() => resolve(a+b)));
 
-const max = async (a, b) => {
-  await timeout(2000);
+const max = (a, b) => new Promise((resolve) => timeout(2000).then(() => resolve(a>b ? a : b)));
 
-  return a > b ? a : b;
-};
-
-const avg = async (a, b) => {
-  await timeout(2000);
-  const s = await sum(a, b);
-  return s / 2;
-};
+const avg = (a, b) => new Promise((resolve) => timeout(2000)
+                                                .then(() => sum(a, b))
+                                                .then((s) => resolve(s/2)));
 
 const obj = {
   name: "Marcus Aurelius",
-  async split(sep = " ") {
-    await timeout(2000);
-
-    return this.name.split(sep);
-  },
+  
+  split(sep = " ") {
+    return new Promise((resolve) => timeout(2000).then(() => resolve(this.name.split(sep))));
+  }
 };
 
 class Person {
@@ -42,22 +29,18 @@ class Person {
     this.name = name;
   }
 
-  static async of(name) {
-    await timeout(2000);
-
-    return new Person(name);
+  static of(name){
+    return new Promise((resolve) => timeout(2000).then(() => resolve(new Person(name))));
   }
 
-  async split(sep = " ") {
-    await timeout(2000);
-
-    return this.name.split(sep);
+  split(sep = " ") {
+    return new Promise((resolve) => timeout(2000).then(() => resolve(this.name.split(sep))));
   }
 }
 
-const person = new Person("Marcus Aurelius");
-
 (async () => {
+  const person = await Person.of("Marcus Aurelius");
+
   console.log("await inc(5) =", await inc(5));
   console.log("await sum(1, 3) =", await sum(1, 3));
   console.log("await max(8, 6) =", await max(8, 6));
