811b41e742285f9614a6eac36534b5b4f09f9921
hardi09 <hardimajmundar0906@gmail.com>
2022-11-11 13:13:01 -0500

    done activity

--- module-08/sync.js
+++ module-08/sync.js
@@ -1,18 +1,45 @@
+const { resolve } = require("path");
+
+const wait = (ms=2000)=> new Promise((resolve)=> setTimeout(resolve,ms));
+
+const ins = async(a)=>{
+  await wait();
+  return a +1;
+}
+//promise
+const ins2 = (a)=> new Promise((resolve)=> wait().then(()=> resolve(a+ 1)));
+
 function inc(a) {
   return a + 1;
 }
+
+//promises
+const sum2 = (a,b) => new Promise((resolve)=> wait().then(()=> resolve(a+b)));
 
 const sum = function (a, b) {
   return a + b;
 };
 
+
+const max2 = (a,b)=> new Promise((resolve)=> wait().then(()=> resolve(a > b ? a :b)));
+
 const max = (a, b) => (a > b ? a : b);
+
+//promises
+const avg2 = (a,b) => new Promise((resolve) => wait()
+                                  .then(()=> sum2(a ,b))
+                                  .then((s)=> resolve (s/2)));
 
 const avg = (a, b) => {
   const s = sum(a, b);
   return s / 2;
 };
-
+const obj2 = {
+  name: "Marcus Aurelius",
+  split(sep = " ") {
+    return new Promise((resolve) => wait().then(() => resolve(this.name.split(sep))))
+  },
+};
 const obj = {
   name: "Marcus Aurelius",
   split(sep = " ") {
@@ -26,19 +53,39 @@ class Person {
   }
 
   static of(name) {
-    return new Person(name);
+    return new Promise((resolve)=> wait().then(()=> resolve(new Person(name))));
+  //  return new Person(name);
   }
 
   split(sep = " ") {
-    return this.name.split(sep);
+    //return this.name.split(sep);
+    return new Promise((resolve) => wait().then(() => resolve(this.name.split(sep))))
   }
 }
 
-const person = Person.of("Marcus Aurelius");
 
-console.log("inc(5) =", inc(5));
-console.log("sum(1, 3) =", sum(1, 3));
-console.log("max(8, 6) =", max(8, 6));
-console.log("avg(8, 6) =", avg(8, 6));
-console.log("obj.split() =", obj.split());
-console.log("person.split() =", person.split());
+
+// const main = async () => {
+
+//   const person = await Person.of("Marcus Aurelius");
+
+//   console.log("inc(5) =", await ins2(5));
+//   console.log("sum(1, 3) =", await sum2(1, 3));
+//   console.log("max(8, 6) =", await max2(8, 6));
+//   console.log("avg(8, 6) =", await avg2(8, 6));
+//   console.log("obj.split() =", await obj.split());
+//   console.log("person.split() =", await person.split());
+// }
+
+// main()
+
+
+ins2(6).then((value)=> console.log(`inc(6)  = ${value}`));
+sum2(2,2).then((value)=>console.log(`sum2(2,2) = ${value}`));
+max2(4,8).then((value)=>console.log(`max2(8,4) is  = ${value}`));
+avg2(4,8).then((value)=>console.log(`avg2(8,4) is  = ${value}`));
+Person.of("Marcus Aurelius")
+.then((person)=> person.split())
+.then((value)=>console.log(`person.split() = ${value}`))
+obj2.split().then((value)=>console.log(`object.split() is  = ${value}`))
+
