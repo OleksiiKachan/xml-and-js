52583a836db299d6c1199bf8a74164413bc8e3e8
janki patel <jankipatel4511@gmail.com>
2022-11-12 23:08:42 -0500

    done

--- module-08/sync.js
+++ module-08/sync.js
@@ -1,22 +1,40 @@
-function inc(a) {
-  return a + 1;
+const wait = (ms = 2000) => new Promise((resolve)=>setTimeout(resolve, ms));
+
+const inc = async(a)=>{
+  await wait();
+  return a+1;
 }
-
-const sum = function (a, b) {
-  return a + b;
-};
-
-const max = (a, b) => (a > b ? a : b);
-
-const avg = (a, b) => {
-  const s = sum(a, b);
+const inc2 = (a)=> new Promise((resolve) =>wait().then(()=>resolve(a+1)));
+const sum = async(a,b)=>
+{
+  await wait();
+  return a+b;
+}
+const sum2 = (a,b)=> new Promise((resolve) =>wait().then(()=>resolve(a+b)));
+const max = async(a,b)=>{
+  await wait;
+  return (a > b ? a : b);
+}
+const max2 = (a,b)=> new Promise((resolve) =>wait().then(()=>resolve(a > b ? a : b)));
+const avg = async (a, b) => {
+  await wait;
+  const s = await sum(a, b);
   return s / 2;
 };
+
+const avg2 = (a,b)=> new Promise((resolve) =>wait()
+.then(()=>sum(a,b)).
+then((s)=>resolve(s/2)));
+
 
 const obj = {
   name: "Marcus Aurelius",
   split(sep = " ") {
-    return this.name.split(sep);
+    // return this.name.split(sep);
+    return new Promise((resolve)=>
+  {
+    wait().then(()=>resolve(this.name.split(sep)));
+  });
   },
 };
 
@@ -25,20 +43,37 @@ class Person {
     this.name = name;
   }
 
-  static of(name) {
-    return new Person(name);
+  static async of(name) {
+    return new Promise((resolve)=>
+  {
+    wait().then(()=>resolve(new Person(name)));
+  });
   }
 
-  split(sep = " ") {
-    return this.name.split(sep);
-  }
+  async split(sep = " ") {
+    return new Promise((resolve)=>
+  {
+    wait().then(()=>resolve(this.name.split(sep)));
+  });
+
+ }
 }
 
-const person = Person.of("Marcus Aurelius");
 
-console.log("inc(5) =", inc(5));
-console.log("sum(1, 3) =", sum(1, 3));
-console.log("max(8, 6) =", max(8, 6));
-console.log("avg(8, 6) =", avg(8, 6));
-console.log("obj.split() =", obj.split());
-console.log("person.split() =", person.split());
+inc2(5).then((value)=>console.log(`inc(5)= ${value}`));
+sum2(5,3).then((value)=>console.log(`sum(5,3)= ${value}`));
+max2(5,3).then((value)=>console.log(`max(5,3)= ${value}`));
+
+avg2(5,3).then((value)=>console.log(`avg(5,3)= ${value}`));
+obj.split().then((value)=>console.log(`obj.split()= ${value}`));
+// Person.of('janki'.split()).then((value)=>console.log(`person.split()= ${value}`));
+Person.of("janki").then(Person=>Person.split().then((value)=>console.log(`person = ${value}`)));
+(async()=>{
+  const person = await Person.of("Marcus Aurelius");
+console.log("inc(5) =", await inc(5));
+console.log("sum(1, 3) =", await sum(1, 3));
+console.log("max(8, 6) =", await max(8, 6));
+console.log("avg(8, 6) =", await avg(8, 6));
+console.log("obj.split() =", await obj.split());
+console.log("person.split() =", await person.split());
+})();
