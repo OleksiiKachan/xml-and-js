83470c431da11eeb5b70f365818727da129bc8ca
Heli kheni <helipatel56789@gmail.com>
2022-11-13 23:48:43 -0500

    complete assignment

--- module-08/cb-hell.js
+++ module-08/cb-hell.js
@@ -4,53 +4,40 @@
  * 3. Make sure the "Finish" is logged after all the data is converted
  */
 
-function timeout(ms, callback) {
-  return new Promise(function (resolve) {
-    setTimeout(function () {
-      resolve();
-      callback();
-    }, ms);
-  });
+const timeout2 = (ms = 1000) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const generateRandomNumber2 = () => Math.floor(Math.random() * 40);
+
+  const generateData2 = async () => {
+    await timeout2(1000,()=>{});
+    const data = Array.from({ length: 20 }, generateRandomNumber2);
+    return data;
+  }
+
+  const convertToFeet2 = async (meters) => {
+    const feet = meters * 3.2808;
+    timeout2(3500, function () {
+      callback(feet);
+    });
+    await timeout2(3500);
+    logResult2(meters, feet)
+  }
+
+const processData2 = async (data) => {
+  await Promise.all(data.map(value => convertToFeet2(value))); 
 }
 
-function generateRandomNumber() {
-  return Math.floor(Math.random() * 40);
-}
-
-function generateData(callback) {
-  timeout(1000, function () {
-    const data = Array.from({ length: 20 }, generateRandomNumber);
-    callback(data);
-  });
-}
-
-function convertToFeet(meters, callback) {
-  const feet = meters * 3.2808;
-  timeout(3500, function () {
-    callback(feet);
-  });
-}
-
-function processData(data, callback) {
-  data.map(function (value) {
-    callback(value);
-  });
-}
-
-function logResult(meters, feet) {
+const logResult2 = (meters, feet) => {
   console.log(`Converted ${meters}m to ${feet}ft`);
 }
 
-function main() {
+const main = async () => {
   console.log("Start");
-  generateData(function (data) {
-    processData(data, function (value) {
-      convertToFeet(value, function (result) {
-        logResult(value, result);
-      });
-    });
-  });
+  const data = await generateData2()
+  await processData2(data)
   console.log("Finish");
 }
 
+console.log("With async");
 main();
+
