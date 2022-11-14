2fb256766548f83d6e32ce7cc16e6a8337544e8a
GKsain8 <105449587+GKsain8@users.noreply.github.com>
2022-11-14 01:09:40 -0500

    module 8/ activity

--- module-08/sync.js
+++ module-08/sync.js
@@ -42,3 +42,69 @@ console.log("max(8, 6) =", max(8, 6));
 console.log("avg(8, 6) =", avg(8, 6));
 console.log("obj.split() =", obj.split());
 console.log("person.split() =", person.split());
+
+//method2 Activity(module 8)
+
+const wait = (ms = 2000) => new Promise((resolve) => setTimeout(resolve,ms));
+const inc2 = (a) => 
+  new Promise((resolve)=>{
+    wait().then(()=> resolve(a+1));
+  })
+
+const sum2 = (a,b) => {
+  return new Promise((resolve) => {
+    wait().then(() => resolve(a+b));
+  })
+}
+
+const max2 = (a,b) => {
+  return new Promise((resolve) => {
+    wait().then(() => resolve(a > b ? a : b));
+  })
+}
+
+const avg2 = (a,b) => {
+  return new Promise((resolve) => {
+    wait().then(() => sum(a,b))
+    .then((s)=> resolve(s/2))
+  });
+}
+
+const obj2 = {
+  name: "Marcus Aurelius",
+  split(sep = " ") {
+    return new Promise((resolve)=> {
+      wait().then(()=> resolve(this.name.split(sep)) );
+    })
+  },
+};
+
+class Person2 {
+  constructor(name) {
+    this.name = name;
+  }
+
+  static of(name) {
+    return new Promise((resolve) => 
+    wait().then(()=> resolve (new Person(name))));
+  }
+
+  split(sep = " ") {
+    return new Promise((resolve)=> {
+      wait().then(() => resolve(this.name.split(sep)));
+    })
+  }
+}
+
+const main = async()=>{
+  const person = await Person.of("Marcus Aurelius");
+  console.log(person);
+  console.log("inc(5) =", await inc2(5));
+  console.log("sum(1, 3) =", await sum2(1, 3));
+  console.log("max(8, 6) =", await max2(8, 6));
+  console.log("avg(8, 6) =", await avg2(8, 6));
+  console.log("obj.split() =", await obj2.split());
+  console.log("person.split() =", await person2.split());
+};
+
+main();
