27212827d28dd436de9d36ba583e08d4ffc40b2f
Onwah <108599340+Onwah@users.noreply.github.com>
2022-11-14 09:14:50 -0500

    Add Assignment

--- module-08/cb-hell.js
+++ module-08/cb-hell.js
@@ -3,8 +3,9 @@
  * 2. Fix callback hell by rewriting it with async/await
  * 3. Make sure the "Finish" is logged after all the data is converted
  */
+ const timeout = (ms = 1000) => new Promise((resolve)=> setTimeout(resolve,ms));
 
-function timeout(ms, callback) {
+/*function timeout(ms, callback) {
   return new Promise(function (resolve) {
     setTimeout(function () {
       resolve();
@@ -12,7 +13,14 @@ function timeout(ms, callback) {
     }, ms);
   });
 }
+*/
 
+/*const generateRandomNumber = async () =>
+await timeout().then (()=> (Math.floor(Math.random() * 40)));
+*/
+
+const generateRandomNumber = () => Math.floor(Math.random() * 40);
+/*
 function generateRandomNumber() {
   return Math.floor(Math.random() * 40);
 }
@@ -24,23 +32,55 @@ function generateData(callback) {
   });
 }
 
+*/
+
+const generateData = async () => {
+await timeout()
+return Array.from({ length: 20 }, generateRandomNumber);
+    
+}
+
+/*
 function convertToFeet(meters, callback) {
   const feet = meters * 3.2808;
   timeout(3500, function () {
     callback(feet);
   });
 }
+*/
 
+const convertToFeet = async(meters) => {
+  const feet = meters * 3.2808;
+  await timeout();
+  logResult(meters, feet)
+
+}
+
+/*
 function processData(data, callback) {
   data.map(function (value) {
     callback(value);
   });
 }
 
+*/
+const processData = async (data) => {
+
+  await timeout(4000).then (data.map(value => convertToFeet(value)));
+}
+
+/*
 function logResult(meters, feet) {
   console.log(`Converted ${meters}m to ${feet}ft`);
 }
+*/
 
+const logResult = (meters, feet) => {
+  console.log(`Converted ${meters}m to ${feet}ft`);
+}
+
+
+/*
 function main() {
   console.log("Start");
   generateData(function (data) {
@@ -51,6 +91,19 @@ function main() {
     });
   });
   console.log("Finish");
+}
+
+main();
+*/
+
+const main = async() => {
+
+  console.log("Start");
+  const data = await generateData()
+  await processData(data)
+
+  console.log("Finish");
+
 }
 
 main();
