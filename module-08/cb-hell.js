244f72c0b67a9c8a7d77c387d515134ed175ec8a
Ekampreet Singh <es234>
2022-11-14 02:53:02 -0500

    Added Assignment solution

--- module-08/cb-hell.js
+++ module-08/cb-hell.js
@@ -4,53 +4,140 @@
  * 3. Make sure the "Finish" is logged after all the data is converted
  */
 
-function timeout(ms, callback) {
-  return new Promise(function (resolve) {
-    setTimeout(function () {
-      resolve();
-      callback();
-    }, ms);
-  });
-}
+// function timeout(ms, callback) {
+//   return new Promise(function (resolve) {
+//     setTimeout(function () {
+//       resolve();
+//       callback();
+//     }, ms);
+//   });
+// }
 
-function generateRandomNumber() {
-  return Math.floor(Math.random() * 40);
-}
+const timeout = (ms,callback) => new Promise( resolve => { 
+  setTimeout( ()=> {
+    resolve(); 
+    callback();
+  }, ms) });
 
-function generateData(callback) {
-  timeout(1000, function () {
-    const data = Array.from({ length: 20 }, generateRandomNumber);
-    callback(data);
-  });
-}
 
-function convertToFeet(meters, callback) {
-  const feet = meters * 3.2808;
-  timeout(3500, function () {
-    callback(feet);
-  });
-}
 
-function processData(data, callback) {
-  data.map(function (value) {
-    callback(value);
-  });
-}
+// function generateRandomNumber() {
+//   return Math.floor(Math.random() * 40);
+// }
 
-function logResult(meters, feet) {
-  console.log(`Converted ${meters}m to ${feet}ft`);
-}
+const generateRandomNumber = () => Math.floor(Math.random() * 40);
 
-function main() {
-  console.log("Start");
-  generateData(function (data) {
-    processData(data, function (value) {
-      convertToFeet(value, function (result) {
-        logResult(value, result);
-      });
+
+
+// function generateData(callback) {
+//   timeout(1000, function () {
+//     const data = Array.from({ length: 20 }, generateRandomNumber);
+//     callback(data);
+//   });
+// }
+
+// const generateData = callback => { 
+//   timeout(1000, () => { 
+//     const data = Array.from({ length: 20 }, generateRandomNumber); 
+//     callback(data);
+//   })
+// }
+
+const generateData = () => new Promise( resolve => { 
+    timeout(1000, () => { 
+      const data = Array.from({ length: 20 }, generateRandomNumber); 
+      resolve(data);
     });
+  } 
+);
+
+
+
+// function convertToFeet(meters, callback) {
+//   const feet = meters * 3.2808;
+//   timeout(3500, function () {
+//     callback(feet);
+//   });
+// }
+
+// const convertToFeet = (meters, callback) => {
+//   const feet = meters * 3.2808;
+//   timeout(3500, () => {
+//     callback(feet);
+//   });
+// }
+
+const convertToFeet = (meters) => new Promise( resolve => {
+  const feet = meters * 3.2808;
+  timeout(3500, () => {
+    resolve(feet);
   });
+})
+
+
+// function processData(data, callback) {
+//   data.map(function (value) {
+//     callback(value);
+//   });
+// }
+
+// const processData = (data, callback) => {
+//   data.map( value => {
+//     callback(value);
+//   })
+// }
+
+const processData = (data, callback) => new Promise(
+  (resolve) => {
+    const converted = Promise.all(data.map(callback));
+    resolve(converted);
+  }
+)
+
+
+// function logResult(meters, feet) {
+//   console.log(`Converted ${meters}m to ${feet}ft`);
+// }
+
+const logResult = (metersArr,feetArr) => {
+  for(let index in metersArr)
+    console.log(`Converted ${metersArr[index]}m to ${feetArr[index]}ft`);
+}
+
+
+
+// function main() {
+//   console.log("Start");
+//   generateData(function (data) {
+//     processData(data, function (value) {
+//       convertToFeet(value, function (result) {
+//         logResult(value, result);
+//       });
+//     });
+//   });
+//   console.log("Finish");
+// }
+
+// const main = () => {
+//   console.log("Start");
+//   generateData( (data) => {
+//     processData(data, (value) => {
+//       convertToFeet(value, (result) => {
+//         logResult(value, result);
+//       });
+//     });
+//   });
+//   console.log("Finish");
+// }
+
+const main = async() => {
+  console.log("Start");
+  const dataArr = await generateData();
+  const resultArr = await processData(dataArr, convertToFeet);
+  logResult(dataArr, resultArr);
   console.log("Finish");
 }
+
+
 
 main();
