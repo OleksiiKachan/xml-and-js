9e0a1a00aebbf2d6d747ce308905bbdf0cc95cd8
Jay Savani <jaysavani@Jays-MBP.ht.home>
2022-11-14 22:57:14 -0500

    Module: 8 - Assignmnet

---
+++ module-08/assignments/cb-hell.js
@@ -0,0 +1,41 @@
+/**
+ * 1. Replace regular functions with arrow functions
+ * 2. Fix callback hell by rewriting it with async/await
+ * 3. Make sure the "Finish" is logged after all the data is converted
+ */
+
+const timeout = (ms) =>  new Promise((resolve) => {
+    setTimeout(() => {
+    resolve();
+  }, ms);
+});
+
+const generateRandomNumber = () =>  Math.floor(Math.random() * 40);
+
+const generateData = async() => {
+  await timeout(1000)
+  return Array.from({ length: 20 }, generateRandomNumber);
+}
+
+const convertToFeet = async(meters) => {
+  const feet = meters * 3.2808;
+  await timeout(3500)
+  return feet
+}
+
+function logResult(meters, feet) {
+  console.log(`Converted ${meters}m to ${feet}ft`);
+}
+
+const main = async() => {
+  console.log("Start");
+  const data = await generateData();
+  for(let i = 0; i < data.length; i++){
+  const x = data[i];
+  const result = await convertToFeet(x);
+  logResult(x, result);
+}
+console.log("Finish");
+}
+
+main();
