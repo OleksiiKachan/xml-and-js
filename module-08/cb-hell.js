f0aea196de72638f2493e523e95001137f03f46e
Bhavish Rohilla <bhavishrohilla59@gmail.com>
2022-11-15 11:29:33 -0500

    module-08 assignment completed

--- module-08/cb-hell.js
+++ module-08/cb-hell.js
@@ -4,53 +4,35 @@
  * 3. Make sure the "Finish" is logged after all the data is converted
  */
 
-function timeout(ms, callback) {
-  return new Promise(function (resolve) {
-    setTimeout(function () {
-      resolve();
-      callback();
-    }, ms);
-  });
+ const timeout = (ms = 1000) => new Promise((resolve) => setTimeout(resolve, ms));
+
+ const generateRandomNumber = () => Math.floor(Math.random() * 40);
+
+
+ const generateData = async () => {
+  await timeout()
+  return Array.from({ length: 20 }, generateRandomNumber)
 }
 
-function generateRandomNumber() {
-  return Math.floor(Math.random() * 40);
-}
-
-function generateData(callback) {
-  timeout(1000, function () {
-    const data = Array.from({ length: 20 }, generateRandomNumber);
-    callback(data);
-  });
-}
-
-function convertToFeet(meters, callback) {
+const convertToFeet = async (meters) => {
   const feet = meters * 3.2808;
-  timeout(3500, function () {
-    callback(feet);
-  });
+  await timeout(3500);
+  logResult(meters, feet)
+}
+const processData = async (data) => {
+  await Promise.all(data.map(value => convertToFeet(value)));
 }
 
-function processData(data, callback) {
-  data.map(function (value) {
-    callback(value);
-  });
-}
-
-function logResult(meters, feet) {
+const logResult = (meters, feet) => {
   console.log(`Converted ${meters}m to ${feet}ft`);
 }
 
-function main() {
+const main = async () => {
   console.log("Start");
-  generateData(function (data) {
-    processData(data, function (value) {
-      convertToFeet(value, function (result) {
-        logResult(value, result);
-      });
-    });
-  });
+  const data = await generateData()
+  await processData(data)
+
+
   console.log("Finish");
 }
-
 main();
