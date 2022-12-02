e078024e888b7e44b5d9d6e2514ca4418992d56f
Vinay Muthyapu <muthyapu.vinay@gmail.com>
2022-12-02 15:38:02 -0500

    module10 activity submission

---
+++ Quiz3/Quiz3.js
@@ -0,0 +1,17 @@
+const main = async() => {
+
+    const data = await loadData();
+    
+    
+    
+    console.log(data.length);
+    
+    };
+    
+    
+    
+    const length = main();
+    
+    
+    
+    console.log(length);
