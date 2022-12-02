c5db2b81b686b65825985e46bedda2fb78d43766
Heli kheni <helipatel56789@gmail.com>
2022-12-02 13:18:56 -0500

    done act

---
+++ module-10/assignments/activity.js
@@ -0,0 +1,79 @@
+const clientId = `b4621a5afc324db68d001d2b1bcb638b`;
+const clientSecret = `ea4eec3c3b4c4be4823ebc6606e15c59`;
+
+const getToken = async () => {
+  const result = await fetch("https://accounts.spotify.com/api/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization: "Basic " + btoa(clientId + ":" + clientSecret),
+    },
+    body: "grant_type=client_credentials",
+  });
+
+  const data = await result.json();
+  return data.access_token;
+};
+
+const getGenres = async (token) => {
+  const result = await fetch(
+    `https://api.spotify.com/v1/browse/categories?locale=sv_US`,
+    {
+      method: "GET",
+      headers: { Authorization: "Bearer " + token },
+    }
+  );
+
+  const data = await result.json();
+  return data.categories.items;
+};
+
+const getPlaylistByGenre = async (token, genreId) => {
+  const limit = 10;
+
+  const result = await fetch(
+    `https://api.spotify.com/v1/browse/categories/${genreId}/playlists?limit=${limit}`,
+    {
+      method: "GET",
+      headers: { Authorization: "Bearer " + token },
+    }
+  );
+
+  const data = await result.json();
+  return data.playlists.items;
+};
+
+const loadGenres = async () => {
+  const token = await getToken();
+  const genres = await getGenres(token);
+  const list = document.getElementById(`genres`);
+  genres.map(async ({ name, id, icons: [icon], href }) => {
+    const playlists = await getPlaylistByGenre(token, id);
+    const playlistsList = playlists
+      .map(
+        ({ name, external_urls: { spotify }, images: [image] }) => `
+        <li>
+          <a href="${spotify}" alt="${name}" target="_blank">
+            <img src="${image.url}" width="180" height="180"/>
+          </a>
+        </li>`
+      )
+      .join(``);
+
+    if (playlists) {
+      const html = `
+      <article class="genre-card">
+        <img src="${icon.url}" width="${icon.width}" height="${icon.height}" alt="${name}"/>
+        <div id='in'>
+          <h2>${name}</h2>
+          <ol> ${playlistsList} </ol>
+        
+        </div>
+      </article>`;
+
+      list.insertAdjacentHTML("beforeend", html);
+    }
+  });
+};
+
+loadGenres();
