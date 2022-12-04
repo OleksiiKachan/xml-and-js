144fe64d09e6680c533b5fffc185c941ac9392bf
Ekampreet Singh <es234>
2022-12-04 12:19:29 -0500

    Added assignment solution

--- module-10/assignments/app.js
+++ module-10/assignments/app.js
@@ -30,7 +30,7 @@ const getToken = async () => {
   }
   
   const getPlaylistByGenre = async (token, genreId) => {
-    const limit = 9;
+    const limit = 3;
   
     const result = await fetch(`https://api.spotify.com/v1/browse/categories/${genreId}/playlists?limit=${limit}`, {
       method: 'GET',
@@ -44,6 +44,37 @@ const getToken = async () => {
     return data.playlists ? data.playlists.items : [];
   }
   
+  const getTracksByPlaylist = async(token, api) => {
+    const limit = 5;
+    
+    const result = await fetch(api + `?limit=${limit}`,{
+      method: "GET",
+      headers:{
+        Authorization: "Bearer " + token
+      }
+    })
+
+    const data = await result.json();
+
+    return data.items;
+  }
+
+
+
+  const millisecondsToMinutesAndSeconds = (ms) => {
+    const minutes = Math.floor( ms / 60000 );
+    const seconds = (( ms % 60000) / 1000 ).toFixed(0);
+    return ( 
+      ( seconds == 60 ) ? ( minutes + 1 ) + ":00" 
+      : ( seconds < 10 ) ? minutes + ":0" + seconds 
+      : minutes + ":" + seconds
+    ); 
+            
+  }
+
+
+
+  
   const loadGenres = async () => {
     const token = await getToken()
     const genres = await getGenres(token);
@@ -54,13 +85,52 @@ const getToken = async () => {
   
     genres.map( async ({name, icons: [icon], id}) => {
       const playlists = await getPlaylistByGenre(token, id)
-    //   console.log(playlists)
+      // console.log(playlists)
   
+
       if (playlists.length) {
-        const playlistItem = playlists.map(
-          ({ name, external_urls: { spotify }, images: [image] }) => 
-          `<li><a href="${spotify}"><img src="${image.url}" width="180" height="180" alt="${name}" </li>`)
-          .join(``)
+
+        let playlistItem = await Promise.all(playlists.map(
+          async({ name, external_urls: { spotify }, images: [image], tracks }) => {
+            // console.log(tracks);
+            const tracksList = await getTracksByPlaylist(token, tracks.href);
+            // console.log(tracksList);
+
+            const tracksHtml = tracksList.reduce((acc, {track})=> {
+              // console.log( track.name);
+              const artistsNames = track.artists.reduce((acc, {name}) => acc === "" ? name : acc + ", " + name , "");
+              
+              // console.log(track.name + "|" +  artistsNames + "|" + millisecondsToMinutesAndSeconds(track.duration_ms));
+              return acc + `
+              <li>
+                <img src="./play-button.png" width="30px" height="30px" alt="Play button" />
+                <div>
+                  <span class="trackName">${track.name}</span>
+                  <span class="trackDuration">&nbsp;&nbsp;&nbsp;&nbsp; ${millisecondsToMinutesAndSeconds(track.duration_ms)}</span>
+                  <br>
+                  <span class="trackArtists">Artists: ${artistsNames}<span>
+                </div>
+              </li>
+              `;
+            }, "");
+            
+            return `
+            <li>
+              <a href="${spotify}">
+                <img src="${image.url}" width="180" height="180" alt="${name}" />
+                <div class="tracksOLWrapper">
+                  <ol class="tracksOL">
+                    ${tracksHtml}
+                  </ol>
+                </div>
+              </a>
+            </li>`
+          }
+          ));
+
+          playlistItem = playlistItem.join('');
+
+          console.log(playlistItem);
   
           const html = `
           <article>
@@ -68,7 +138,7 @@ const getToken = async () => {
             <img src="${icon.url}" width="${icon.width}" height="${icon.height} alt="${name}" </img>
             <h2>${name}</h2>
             </div>
-            <ol>
+            <ol class="playlistOL">
               ${playlistItem}
             </ol>
             
