c4111d6652ad4fe360fbb2e4ba97fed1c24cba33
hardi09 <hardimajmundar0906@gmail.com>
2022-12-05 10:04:23 -0500

    Assignment done

--- module-10/spotify/app.js
+++ module-10/spotify/app.js
@@ -43,24 +43,53 @@ const getPlaylistByGenre = async (token, genreId) => {
   return data.playlists.items;
 };
 
+const getTracksFromPlaylist = async (token, tracksUrl) => {
+  const limit = 2;
+
+  const result = await fetch(tracksUrl + `?limit=${limit}`, {
+    method: "GET",
+    headers: { Authorization: "Bearer " + token },
+  });
+
+  const data = await result.json();
+  return data.items;
+};
+
 const loadGenres = async () => {
   const token = await getToken();
   const genres = await getGenres(token);
   const list = document.getElementById(`genres`);
   genres.map(async ({ name, id, icons: [icon]}) => {
     const playlists = await getPlaylistByGenre(token, id);
-    const playlistsList = playlists
-      .map(
-        ({ name, external_urls: { spotify }, images: [image] }) => `
-        <li>
-          <a href="${spotify}" alt="${name}" target="_blank">
-            <img src="${image.url}" width="180" height="180"/>
-          </a>
-        </li>`
-      )
-      .join(``);
+   
 
-    if (playlists) {
+    if (playlists.length) {
+      Promise.all(playlists
+        .map(async ({ name, external_urls: { spotify }, images: [image], tracks }) => {
+          const playlistTracks = await getTracksFromPlaylist(token, tracks.href);
+          let tracksList = '';
+          if (playlistTracks) {
+            tracksList = playlistTracks
+              .map(({ track }) => {
+                const artists = track.artists
+                  .map(({ name }) => name)
+                  .join(', ');
+                return `<li class="track">${track.name} - ${artists}</li>`})
+              .join('');
+          }
+          return `
+            <li class>
+            <a href="${spotify}" alt="${name}" target="_blank">
+              <img src="${image.url}" width="180" height="180"/>
+            </a>
+            <p> Tracks: </p>
+          <ol class="tracks">
+                  ${tracksList}
+                </ol>
+            </li>`;
+        }))
+        .then(playlistsList => playlistsList.join(""))
+        .then(playlistsList => {
       const html = `
       <article>
         <img id="mainImage" src="${icon.url}" width="${icon.width}" height="${icon.height}" alt="${name}"/>
@@ -73,6 +102,7 @@ const loadGenres = async () => {
       </article>`;
 
       list.insertAdjacentHTML("beforeend", html);
+        })
     }
   });
 };
