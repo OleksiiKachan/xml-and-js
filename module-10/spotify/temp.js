bec1d6e2504426e387752b0570d628a2000d1fe1
Sharon Sebastian <sharonsebastian825@gmail.com>
2022-12-05 12:34:22 -0500

    Added assignment10

--- module-10/spotify/temp.js
+++ module-10/spotify/temp.js
@@ -43,36 +43,68 @@ const getPlaylistByGenre = async (token, genreId) => {
   return data.playlists.items;
 };
 
+const getTracksByPlaylist =async(token, tracks)=>{
+  const limit=5;
+
+  const result=await fetch(tracks+ `?limit=${limit}`, 
+  {
+    method:"GET",
+    headers: { Authorization: "Bearer " + token },
+
+  }
+  );
+  const data = await result.json();
+  return data.items;
+}
+
 const loadGenres = async () => {
   const token = await getToken();
   const genres = await getGenres(token);
-  const list = document.getElementById(`genres`);
-  genres.map(async ({ name, id, icons: [icon], href }) => {
+
+  const list = document.getElementById("genres");
+
+  genres.map(async ({ name, id, icons: [icon] }) => {
     const playlists = await getPlaylistByGenre(token, id);
-    const playlistsList = playlists
-      .map(
-        ({ name, external_urls: { spotify }, images: [image] }) => `
-        <li>
-          <a href="${spotify}" alt="${name}" target="_blank">
-            <img src="${image.url}" width="180" height="180"/>
-          </a>
-        </li>`
-      )
-      .join(``);
 
-    if (playlists) {
-      const html = `
-      <article class="genre-card">
-        <img src="${icon.url}" width="${icon.width}" height="${icon.height}" alt="${name}"/>
-        <div>
-          <h2>${name}</h2>
-          <ol>
-            ${playlistsList}
-          </ol>
-        </div>
-      </article>`;
+    if (playlists.length) {
+      Promise.all(playlists
+        .map(async ({ name, external_urls: { spotify }, images: [image], tracks }) => {
+          const playlistTracks = await getTracksByPlaylist(token, tracks.href);
+          let tracksList = '';
+          if (playlistTracks) {
+            tracksList = playlistTracks
+              .map(({ track }) => {
+                const artists = track.artists
+                  .map(({ name }) => name)
+                  .join(', ');
+                return `<li class="track">${track.name} - ${artists}</li>`})
+              .join('');
+          }
+          return `
+            <li class="playlist">
+                <a href="${spotify}" alt="${name}" target="_blank">
+                    <img src="${image.url}" width="180" height="180" alt="${name}"/>
+                </a>
+                <ol class="tracks">
+                  ${tracksList}
+                </ol>
+            </li>`;
+        }))
+        .then(playlistsList => playlistsList.join(""))
+        .then(playlistsList => {
+          const html = `
+            <article class="genre-card">
+                <div>
+                    <h2>${name}</h2>
+                    <img src="${icon.url}" width="${icon.width}" height="${icon.height}" alt="${name}"/>
+                </div>
+                <ol class="playlists">
+                    ${playlistsList}
+                </ol>
+            </article>`;
 
-      list.insertAdjacentHTML("beforeend", html);
+          list.insertAdjacentHTML("beforeend", html);
+        })
     }
   });
 };
