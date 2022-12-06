49af412636e78f8f82821457838449db0f1d6736
janki patel <jankipatel4511@gmail.com>
2022-12-05 22:33:39 -0500

    complete assignment

---
+++ module-10/spotify/ass.js
@@ -0,0 +1,99 @@
+const clientId = `d82cac5e974d45d0861bf2a4df22baff`;
+const clientSecret = `7a5590ea33de462996e98159098122f8`;
+const getToken = async () => {
+  const result = await fetch("https://accounts.spotify.com/api/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization: "Basic " + btoa(clientId + ":" + clientSecret),
+    },
+    body: "grant_type=client_credentials",
+  });
+
+  const data = await result.json();
+  return data.access_token;
+};
+
+const getGenres = async (token) => {
+  const result = await fetch(
+    `https://api.spotify.com/v1/browse/categories?locale=sv_US`,
+    {
+      method: "GET",
+      headers: { Authorization: "Bearer " + token },
+    }
+  );
+
+  const data = await result.json();
+  return data.categories.items;
+};
+
+const getPlaylistByGenre = async (token, genreId) => {
+  const limit = 10;
+
+  const result = await fetch(
+    `https://api.spotify.com/v1/browse/categories/${genreId}/playlists?limit=${limit}`,
+    {
+      method: "GET",
+      headers: { Authorization: "Bearer " + token },
+    }
+  );
+
+  const data = await result.json();
+  return data.playlists.items;
+};
+
+const getTracks = async(token, href) => {
+  const limit = 3;
+  const result = await fetch(href + `?limit=${limit}`, {
+    method: "GET",
+    headers: { Authorization: "Bearer " + token },
+  });
+
+  const data = await result.json();
+  return data.items;
+}
+
+const loadGenres = async () => {
+  const token = await getToken();
+  const genres = await getGenres(token);
+  const list = document.getElementById(`genres`);
+  genres.map(async({name , icons: [icon], id}) => {
+    const playlists = await getPlaylistByGenre(token, id);
+ 
+
+    if(playlists.length){
+        const playlistsList = Promise.all(playlists.map(async({name, external_urls: { spotify }, images: [image], tracks}) =>{
+            const tracksInPlaylists = await getTracks(token, tracks.href);
+            console.log(tracksInPlaylists);
+
+            if(tracksInPlaylists.length){
+                tracksInPlaylistsList = tracksInPlaylists.map( ( { track}) => {
+                    const artist = track.artists.map(({name}) => name);
+                return `
+                <li><a href="${track.external_urls.spotify}"> ${track.name}<br>Artist(s): ${artist}<br>Album: ${track.album.name}<br>Popularity: ${track.popularity}</li><br>`
+                })
+                .join('');
+            }
+            return`
+            <li><a href="${spotify}"><img id="jj" src="${image.url}" width="180" height="180" alt="${name}"/><ol id=tracks>${tracksInPlaylistsList}</ol></li>`;
+        }))    
+        .then(playlistsList => playlistsList.join(""))
+        .then(playlistsList => {
+        const html = `
+            <article>
+                <img src = "${icon.url}" width="${icon.width}" height="${icon.height}"/>
+                <div>
+                    <h2>${name}</h2>
+                    <ol id=playlist>
+                        ${playlistsList}
+                    </ol>
+                </div>
+            </article>`;
+
+            list.insertAdjacentHTML("beforeend", html);
+        })
+    }
+});
+};
+
+loadGenres();
---
+++ project-topics/project-2/clinic.js
@@ -0,0 +1,81 @@
+const clientId = `d82cac5e974d45d0861bf2a4df22baff`;
+const clientSecret = `7a5590ea33de462996e98159098122f8`;
+
+const getToken = async () => {
+  const result = await fetch("https://accounts.spotify.com/api/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization: "Basic " + btoa(clientId + ":" + clientSecret),
+    },
+    body: "grant_type=client_credentials",
+  });
+
+  const data = await result.json();
+  return data.access_token;
+};
+
+const getGenres = async (token) => {
+  const result = await fetch(
+    `https://api.spotify.com/v1/browse/categories?locale=sv_US`,
+    {
+      method: "GET",
+      headers: { Authorization: "Bearer " + token },
+    }
+  );
+
+  const data = await result.json();
+  return data.categories.items;
+};
+
+const getPlaylistByGenre = async (token, genreId) => {
+  const limit = 10;
+
+  const result = await fetch(
+    `https://api.spotify.com/v1/browse/categories/${genreId}/playlists?limit=${limit}`,
+    {
+      method: "GET",
+      headers: { Authorization: "Bearer " + token },
+    }
+  );
+
+  const data = await result.json();
+  return data.playlists.items;
+};
+
+const loadGenres = async () => {
+  const token = await getToken();
+  const genres = await getGenres(token);
+  const list = document.getElementById(`genres`);
+  genres.map(async ({ name, id, icons: [icon], href }) => {
+    const playlists = await getPlaylistByGenre(token, id);
+    const playlistsList = playlists
+      .map(
+        ({ name, external_urls: { spotify }, images: [image] }) => `
+        <li>
+          <a href="${spotify}" alt="${name}" target="_blank">
+            <img src="${image.url}" width="180" height="180"/>
+          </a>
+        </li>`
+      )
+      .join(``);
+
+    if (playlists) {
+      const html = `
+      <article class="genre-card">
+        <div><img src="${icon.url}" width="${icon.width}" height="${icon.height}" alt="${name}"/>
+        </article>
+        
+          <h2>${name}</h2>
+          <ol>
+            ${playlistsList}
+          </ol>
+        </div>
+      `;
+
+      list.insertAdjacentHTML("beforeend", html);
+    }
+  });
+};
+
+loadGenres();
