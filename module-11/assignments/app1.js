5c1e2cc5708312f83d5656ee17ee9943f5cec1df
Marleni <mar_rimari10@outlook.com>
2022-12-12 22:53:33 -0500

    assigment 11

--- module-11/assignments/app1.js
+++ module-11/assignments/app1.js
@@ -44,18 +44,36 @@ const getPlaylistByGenre = async (token, genreId) => {
   return data.playlists ? data.playlists.items : [];
 };
 
+const getTracksFromPlaylist = async (token, tracksUrl) => {
+  const limit = 5;
+
+  const result = await fetch(tracksUrl + `?limit=${limit}`, {
+      method: "GET",
+      headers: { Authorization: "Bearer " + token },
+  });
+
+  const data = await result.json();
+  return data.items;
+};
+
+
 const loadGenres = async () => {
   const token = await getToken();
   const genres = await getGenres(token);
 
   _data = await Promise.all(
-    genres.map(async (genre) => {
-      const playlists = await getPlaylistByGenre(token, genre.id);
+      genres.map(async (genre) => {
+          let playlists = await getPlaylistByGenre(token, genre.id);
+          playlists = await Promise.all(playlists.map(async (playlist) => {
+              const playlistTracks = await getTracksFromPlaylist(token, playlist.tracks.href);
+              return { ...playlist, playlistTracks };
+          }));
 
-      return { ...genre, playlists };
-    })
+          return { ...genre, playlists };
+      })
   );
 };
+
 
 const renderGenres = (filterTerm) => {
   let source = _data;
@@ -69,38 +87,69 @@ const renderGenres = (filterTerm) => {
     });
   }
 
-  const list = document.getElementById(`genres`);
+  const list = document.getElementById("genres");
+  list.innerHTML = "";
+  const opt_genres = document.getElementById("opt_genres").checked;
+  const opt_tracklists = document.getElementById("opt_tracklists").checked;
+  const opt_all = document.getElementById("opt_all").checked;
 
-  const html = source.reduce((acc, { name, icons: [icon], playlists }) => {
-    const playlistsList = playlists
-      .map(
-        ({ name, external_urls: { spotify }, images: [image] }) => `
-        <li>
-          <a href="${spotify}" alt="${name}" target="_blank">
-            <img src="${image.url}" width="180" height="180"/>
-          </a>
-        </li>`
-      )
-      .join(``);
+  source.map(({ name, icons: [icon], playlists }) => {
 
-    if (playlists) {
-      return (
-        acc +
-        `<article class="genre-card">
-        <img src="${icon.url}" width="${icon.width}" height="${icon.height}" alt="${name}"/>
-        <div>
-          <h2>${name}</h2>
-          <ol>
-            ${playlistsList}
-          </ol>
-        </div>
-      </article>`
-      );
-    }
-  },);
+      if (playlists.length) {
+          let playlistsList = '';
 
-  list.innerHTML = html;
-};
+          if (opt_tracklists || opt_all) {
+
+              if (opt_all){
+                  playlists = playlists.slice(0, 9);
+              }
+              playlistsList = playlists
+                  .map(({ name, external_urls: { spotify }, images: [image], playlistTracks }) => 
+                          buildPlaylist(name, spotify, image.url, playlistTracks, opt_all))
+                  .join("");
+              playlistsList = `<ol class="playlists">${playlistsList}</ol>`;
+          }
+
+          const article_class = opt_genres ? 'genre_no_playlists' : 'genre_with_playlists';
+
+          const html = `
+          <article class="${article_class}">
+              <div>
+                  <h2>${name}</h2>
+                  <img src="${icon.url}" width="${icon.width}" height="${icon.height}" alt="${name}"/>
+              </div>
+              ${playlistsList}
+          </article>`;
+
+          list.insertAdjacentHTML("beforeend", html);
+
+      }
+  });
+}
+
+const buildPlaylist = (playlist_name, playlist_url, playlist_image_url, playlistTracks, opt_all) => {
+  let tracksList = '';
+  const tracksListHtml = opt_all ? `<li class="playlist_with_tracks">` : `<li class="playlist_no_tracks">`;
+
+  if (playlistTracks && opt_all) {
+      tracksList = playlistTracks
+          .map(({ track }) => {
+              const artists = track.artists
+                  .map(({ name }) => name)
+                  .join(', ');
+              return `<li class="track">${track.name} - ${artists}</li>`
+          })
+          .join('');
+      tracksList = `<ol class="tracks">${tracksList}</ol>`;
+  }
+
+  return tracksListHtml + `
+      <a href="${playlist_url}" target="_blank">
+          <img src="${playlist_image_url}" width="180" height="180" alt="${playlist_name}"/>
+      </a>
+      ${tracksList}
+  </li>`;
+}
 
 loadGenres().then(renderGenres);
 
@@ -110,4 +159,9 @@ const onSubmit = (event) => {
   const term = event.target.term.value;
 
   renderGenres(term);
+};
+
+const onReset = ()=>{
+  document.getElementById("opt_all").checked = true;
+  renderGenres();
 };
