7a33a9df441fa7e464b254994a15b91d1128f70d
Santosh Dhakal <santosh.dhakal07@gmail.com>
2022-12-15 10:39:34 -0500

    completed currency convert

---
+++ project2/app.js
@@ -0,0 +1,61 @@
+const accessKey = '05eda655830de5fdf3ba53afea1fc41b'
+
+let _data
+
+const loadData = async () => {
+  const result = await fetch(`http://api.coinlayer.com/live?access_key=${accessKey}`)
+
+  _data = await result.json()
+}
+
+const renderData = async () => {
+  const from_select = document.getElementById('from')
+  const to_select = document.getElementById('to')
+
+  const amountFrom = document.getElementById('amount')
+  const convertedTo = document.getElementById('converted-to')
+
+  Object.keys(_data.rates).forEach((crypto) => {
+    const optionForFrom = document.createElement('option')
+    const optionForTo = document.createElement('option')
+    optionForFrom.text = crypto
+    optionForFrom.value = crypto
+  
+    optionForTo.text = crypto
+    optionForTo.value = crypto
+
+    from_select.add(optionForFrom)
+    to_select.add(optionForTo)
+  })
+
+  from_select.value = 'BTC'
+  to_select.value = 'ETH'
+
+  const convert = () => {
+    const fromCurrency = from_select.value
+    const toCurrency = to_select.value
+    const amount = amountFrom.value
+
+    const amountInUSD = _data.rates[fromCurrency] * amount
+    const toAmountRateInUSD = _data.rates[toCurrency]
+
+    if (toAmountRateInUSD !== 0) {
+      const convertedAmount = amountInUSD / toAmountRateInUSD
+      convertedTo.value = convertedAmount
+    } else {
+      convertedTo.value = 0
+    }
+  }
+
+  amount.addEventListener('input', convert)
+
+  from_select.addEventListener('change', convert)
+  to_select.addEventListener('change', convert)
+}
+
+const main = async () => {
+  await loadData()
+  renderData()
+}
+
+main()
