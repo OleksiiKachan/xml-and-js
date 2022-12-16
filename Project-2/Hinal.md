639425eb16eb92b718bf92c41c174cf7a70b334e
Hinal20 <107871243+Hinal20@users.noreply.github.com>
2022-12-15 21:39:21 -0500

    Add project

---
+++ Project-2/Hinal.md
@@ -0,0 +1,3 @@
+Hinal Amin(n01530824)
+
+[-x-] I have worked on css and worked on search option in js.
---
+++ Project-2/cat.js
@@ -0,0 +1,79 @@
+var token = 'live_1EeIlchSbiPqtqiQKzkGTusxyD9IakCtsEBlF35SFC2jymIfr1ARqibsKGvaI4nD';
+let _data = [];
+
+const getlogos = async (token, term) => {
+  const result = await fetch(
+    `https://api.thecatapi.com/v1/images/search?query=${term}`,
+    {
+      method: "GET",
+      headers: { Authorization: "Bearer " + token },
+    }
+   
+  );
+   _data = await result.json();
+  return _data.images ;
+};
+
+
+
+
+
+const renderHTML =  (term) => {
+  const htmlBody = document.getElementById("genres");
+  let source = _data;
+  if (term) { 
+      htmlBody.innerHTML = "";
+        source.map(({id, url})=>{
+        let html = `
+        
+        <img class="icon" src="${url}"/> 
+        
+        
+        <div class="domain">Id: ${id}</div>
+   
+    
+        `; 
+        htmlBody.insertAdjacentHTML("beforeend", html);
+      })
+  }
+  else
+  {
+    htmlBody.innerHTML = "";
+        let html = `
+        <div class="card">
+        </div>
+        `;
+        htmlBody.insertAdjacentHTML("beforeend", html);
+  }
+};
+ console.log(`data rendered`);
+const onSubmit = (event) => {
+  event.preventDefault();
+  const term = event.target.term.value;
+  getlogos(token , term);
+  renderHTML(term);
+};
+
+const onReset = () => {
+  renderHTML();
+};
+fetch("https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=live_1EeIlchSbiPqtqiQKzkGTusxyD9IakCtsEBlF35SFC2jymIfr1ARqibsKGvaI4nD").then((data)=>{
+  console.log(data);
+  return data.json();
+}).then((objectData)=>{
+  console.log(objectData[0].breeds[0].name);
+  console.log(objectData);
+  let tableData="";
+  objectData.map((values)=>{
+    tableData+=`<tr>
+    <td>${values.height}</td>
+    <td>${values.id}</td>
+    <td><img src="${values.url}"/></td>
+    <td>${values.width}</td>
+    <td>${values.breeds[0].name}</td>
+  </tr>`;
+  });
+  document.getElementById("table_body").
+  innerHTML=tableData;
+})
+  
