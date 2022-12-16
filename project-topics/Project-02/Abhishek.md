0c3c99ae382ec730623d03fb2fba817da1388178
Abhishek78788 <pandyaabhi56@gmail.com>
2022-12-15 23:47:05 -0500

    project-02

---
+++ project-topics/Project-02/Abhishek.md
@@ -0,0 +1,6 @@
+# Project -1
+
+# Gropu -8
+## Topic- Clinic
+
+- Helped the fellow member to create scrip file in javascript and created the css
---
+++ project-topics/Project-02/script.js
@@ -0,0 +1,55 @@
+// const API_KEY = ''
+
+// var start = document.getElementById("start").value;
+
+// console.log(start);
+
+const getResults = (searchQuery) => {
+  fetch(
+    `https://api.fda.gov/drug/label.json?search=effective_time:[20110601+TO+20121231]&limit=1`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      // console.log(data.results)
+      const list = document.getElementById("results");
+      list.innerHTML = "";
+      data.results.map((item) => {
+        console.log(item);
+        const html = `
+                <div class="card">
+                <h2>Drug_id: ${item.id}</h2>
+            <h3>Effective time:${item.effective_time}</h3>
+            <h3>Drug details:</h3>
+            <br>
+            <h4>Active Ingredients</h4>
+            <p>${item.active_ingredient}</p>
+            <h4>Inactive Ingredients</h4>
+            <p>${item.inactive_ingredient}</p>
+
+            <h4>Dosage and administratons</h4>
+            <p>${item.dosage_and_administration}</p>
+            <h4>Indication and usage</h4>
+            <p>${item.indications_and_usage}</p>
+            <h4>Other safety information</h4>
+            <p>${item.other_safety_information}</p>
+            <h4>Purpose</h4>
+            <p>${item.purpose}</p>
+            <h4>References</h4>
+            <p>${item.references}</p>
+            <h4>Product elements</h4>
+            <p>${item.spl_product_data_elements}</p>
+            <h4>Stop_use</h4>
+            <p>${item.stop_use}</p>
+            <h4>Warnings</h4>
+            <p>${item.warnings}</p>
+                    
+            <h4>${item.active_ingredient}</h4>
+
+            </div>
+            `;
+
+        list.insertAdjacentHTML("beforeend", html);
+      });
+    });
+};
+getResults("title");
