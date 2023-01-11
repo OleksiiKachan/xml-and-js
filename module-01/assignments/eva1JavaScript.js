a9b8a60aa0af00c8a248f21095c5be6da56cd567
Krushal Dobariya <krushaldobariya17@gmail.com>
2023-01-11 09:41:19 -0500

    add module-1

---
+++ module-01/assignments/eva1JavaScript.js
@@ -0,0 +1,4 @@
+function displayName(){
+    let myname = 'Krushal Dobariya';
+    document.getElementById('name').innerHTML = myname;
+}
---
+++ module-01/assignments/eva2JavaScript.js
@@ -0,0 +1,4 @@
+function displayAll(){
+    let mymail = '';
+    document.getElementById('detail').value;
+}
