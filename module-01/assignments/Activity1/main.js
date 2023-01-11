e50f13b02c5307bbeb80a501939eb4643455ef56
leonvictor104 <113113712+leonvictor104@users.noreply.github.com>
2023-01-11 12:08:11 -0500

    Module 1 evaluation

---
+++ module-01/assignments/Activity1/main.js
@@ -0,0 +1,10 @@
+function buttonClick(){
+    let buttonClick=document.getElementById('addname').value;
+    
+}
+
+function displayName(){
+    let display ='My name is Leon';
+    document.getElementById('displayname').innerHTML = display;
+}
+displayName();
