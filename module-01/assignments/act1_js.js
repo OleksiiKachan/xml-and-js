199280c3f959917959a72732ab22d67ac9513d66
manmeet1997 <kaur.manmeet2311@gmail.com>
2023-01-11 12:13:44 -0500

    module 1 eval

---
+++ module-01/assignments/act1_js.js
@@ -0,0 +1,4 @@
+function displayName() 
+{
+    document.getElementById("s1").innerHTML="Manmeet Kaur";
+}
---
+++ module-01/assignments/act2_js.js
@@ -0,0 +1,10 @@
+function feedback() 
+{
+    var yemail = document.getElementById("ye").value;
+    var visit = document.getElementById("dov").value;
+    var ycom = document.getElementById("yc").value;
+    
+    console.log(yemail);
+    console.log(visit);
+    console.log(ycom);
+}
