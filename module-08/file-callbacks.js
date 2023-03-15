da5173fbd4c7f264d0635660ca8646d18a59b7e9
Achewie95 <113118255+Achewie95@users.noreply.github.com>
2023-03-15 14:07:38 -0400

    complete activity

--- module-08/file-callbacks.js
+++ module-08/file-callbacks.js
@@ -1,6 +1,32 @@
 const fs = require("fs");
 let filename = `${__dirname}/books.json`;
 
+const checkIfExists = (filename) => 
+  new Promise((resolve, reject) =>{
+    fs.exists(filename,(exists) => {
+    if (exists){
+      resolve (filename);
+    }
+    else{
+      reject('404: file not found');
+    }
+  });
+});
+
+const checkIfFile = (filename) =>
+  new Promise((resolve,reject) =>{
+    fs.stat(filename, (err, stats) => {
+      if (exists){
+        resolve(filename);
+      }
+      else{
+        reject("This location contains not a file");
+      }
+    });
+  });
+
+
+
 fs.exists(filename, (exists) => {
   if (exists) {
     fs.stat(filename, (err, stats) => {
@@ -21,4 +47,21 @@ fs.exists(filename, (exists) => {
   } else {
     throw new Error("404: file not found");
   }
+});
+
+
+const main = async() =>{
+  try{
+  await checkIfExists(filename);
+  await checkIfFile(filename);
+  const data = await readFile(filename);
+  console.log(data);
+  }
+  catch(error){
+    console.error(error);
+  }
+};
+
+main().then(() => {
+  console.log('after file')
 });
--- module-08/sync.js
+++ module-08/sync.js
@@ -1,22 +1,31 @@
+const timeout = (ms = 1500) => new Promise(resolve => setTimeout(resolve,ms));
+
 function inc(a) {
-  return a + 1;
+  return timeout().then(() => a+1);
 }
 
+// ***ASYNC METHOD OF WRITING
+//async function inc(a){
+// await timeout()
+// return(a+1)
+//}
+
 const sum = function (a, b) {
-  return a + b;
+  return timeout().then(() => a + b);
 };
 
-const max = (a, b) => (a > b ? a : b);
+const max = (a, b) => {
+  return timeout().then(() => (a > b ? a : b));
+};
 
 const avg = (a, b) => {
-  const s = sum(a, b);
-  return s / 2;
+  return timeout().then(() => sum(a,b)).then(s => s/2);
 };
 
 const obj = {
   name: "Marcus Aurelius",
   split(sep = " ") {
-    return this.name.split(sep);
+    return timeout().then(() => this.name.split(sep))
   },
 };
 
@@ -25,20 +34,31 @@ class Person {
     this.name = name;
   }
 
-  static of(name) {
-    return new Person(name);
+  static async of(name) {
+    return timeout().then(() => new Person(name));
   }
 
-  split(sep = " ") {
-    return this.name.split(sep);
+  async split(sep = " ") {
+    return timeout().then(() => this.name.split(sep))
   }
 }
 
 const person = Person.of("Marcus Aurelius");
 
-console.log("inc(5) =", inc(5));
-console.log("sum(1, 3) =", sum(1, 3));
-console.log("max(8, 6) =", max(8, 6));
-console.log("avg(8, 6) =", avg(8, 6));
-console.log("obj.split() =", obj.split());
-console.log("person.split() =", person.split());
+inc(5).then(data => console.log(`inc(5) = ${data}`))
+//console.log("inc(5) =", await inc(5)); ****ASYNC METHOD OF WRITING
+
+sum(1,3).then(data => console.log(`sum(1, 3) = ${data}`))
+// console.log("sum(1, 3) =", await sum(1, 3));
+
+max(8,6).then(data => console.log(`max(8, 6) = ${data}`))
+// console.log("max(8, 6) =", await max(8, 6));
+
+avg(8, 6).then(data => console.log(`avg(8, 6) = ${data}`))
+// console.log("avg(8, 6) =", await avg(8, 6));
+
+obj.split().then(data => console.log(`obj.split() = ${data}`))
+// console.log("obj.split() =", obj.split());
+
+person.then((person) => person.split()).then((data) => console.log(`person.split() = ${data}`))
+// console.log("person.split() =", person.split());
