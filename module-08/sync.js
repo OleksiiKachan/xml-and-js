cc449fda13b0f7d07bedb7c3d2f98997dbc82dfe
Renil Soni <srenil063@gmail.com>
2023-03-16 21:22:49 -0400

    Completed module-8 activity'

--- module-08/sync.js
+++ module-08/sync.js
@@ -1,22 +1,32 @@
-function inc(a) {
-  return a + 1;
+const { resolve } = require("path");
+
+const timeout = (ms=1) => 
+new Promise((resolve) => {
+  setTimeout(function () {
+    resolve();
+  }, ms);
+});
+
+const inc = async (a) => {
+  return await timeout().then(() => a + 1);
 }
 
-const sum = function (a, b) {
-  return a + b;
+const sum = async (a, b) => {
+  return await timeout().then(() => a + b);
 };
 
-const max = (a, b) => (a > b ? a : b);
+const max = async (a, b) => {
+  return await timeout().then(() => a > b ? a : b);
+};
 
-const avg = (a, b) => {
-  const s = sum(a, b);
-  return s / 2;
+const avg = async (a, b) => {
+  return await timeout().then(async () => await sum(a, b) / 2);
 };
 
 const obj = {
   name: "Marcus Aurelius",
-  split(sep = " ") {
-    return this.name.split(sep);
+  async split(sep = " ") {
+    return await timeout().then(() => this.name.split(sep));
   },
 };
 
@@ -29,16 +39,18 @@ class Person {
     return new Person(name);
   }
 
-  split(sep = " ") {
-    return this.name.split(sep);
+  async split(sep = " ") {
+    return await timeout().then(() => this.name.split(sep));
   }
 }
 
 const person = Person.of("Marcus Aurelius");
 
-console.log("inc(5) =", inc(5));
-console.log("sum(1, 3) =", sum(1, 3));
-console.log("max(8, 6) =", max(8, 6));
-console.log("avg(8, 6) =", avg(8, 6));
-console.log("obj.split() =", obj.split());
-console.log("person.split() =", person.split());
+(async () => {
+console.log("inc(5) =", await inc(5));
+console.log("sum(1, 3) =", await sum(1, 3));
+console.log("max(8, 6) =", await max(8, 6));
+console.log("avg(6, 3) =", await avg(6, 3));
+console.log("obj.split() =", await obj.split());
+console.log("person.split() =", await person.split());
+})();
