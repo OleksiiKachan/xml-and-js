252c92b5d54ea7cf96817b5c3dc08faf8473a0ca
SibiSM <sibichakravarthy888@gmail.com>
2023-03-18 17:14:26 -0400

    Committing the file and snapshot for Assignment 8

---
+++ module-08/assignments/assignment.js
@@ -0,0 +1,37 @@
+const timeout = async (ms) => {
+   return setTimeout(() => ms);
+ }
+
+ const generateRandomNumber = () => {
+   return Math.floor(Math.random() * 40);
+ }
+
+ const generateData = async () => {
+   await timeout(1000);
+   return  processData(Array.from({ length: 20 }, generateRandomNumber));
+ }
+
+ const convertToFeet = async (meters) => {
+   await timeout(3500);
+   return logResult(meters, meters * 3.2808);
+ }
+
+ const processData = async (data) => {
+   data.map(async (value) => {
+     return convertToFeet(value);
+   });
+ }
+
+ const logResult = async (meters, feet) => {
+   console.log(`Converted ${meters}m to ${feet}ft`);
+ }
+
+ async function main() {
+    console.log("------------------------------------------");
+   console.log("Start");
+   await generateData();
+   console.log("Finish");
+   console.log("------------------------------------------");
+ }
+
+ main();
