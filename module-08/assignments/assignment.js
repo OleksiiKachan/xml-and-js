926ccfd98f6c1d187164cd7946d296734750fe65
Dharit Patel <bdharit99@gmail.com>
2023-03-18 21:13:48 -0400

    module 8 assignment eval

---
+++ module-08/assignments/assignment.js
@@ -0,0 +1,48 @@
+const timeout = async (ms) => {
+    setTimeout(() => {}, ms);
+  };
+
+
+  const generateRandomNumber = () => 
+  {
+    return Math.floor(Math.random() * 40);
+  };
+
+  
+  const generateData = async () => 
+  {
+    await timeout(1000); 
+    await processData(Array.from({ length: 20 }, generateRandomNumber)); 
+  };
+
+  
+  const convertToFeet = async (meters) => 
+  {
+    const feet = meters * 3.2808;
+    await timeout(3500);
+    await logResult(meters, feet);
+  };
+
+
+  const processData = async (data) => 
+  {
+   
+    data.map(async (value) => {
+      await convertToFeet(value);
+    });
+  };
+
+  const logResult = async (meters, feet) => 
+  {
+   
+    console.log(`Converted ${meters}m to ${feet}ft`);
+  };
+
+  const main = async () => 
+  {
+    console.log("Start");
+    await generateData();
+    console.log("Finish");
+  };
+
+  main();
