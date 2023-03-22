874be7d0eaaf4814f8d0746ae394039ff42682e6
SibiSM <sibichakravarthy888@gmail.com>
2023-03-22 12:51:07 -0400

    Committing the files for Activity 9

---
+++ module-09/assignments/people-xhr.js
@@ -0,0 +1,52 @@
+const xhttp = new XMLHttpRequest();
+       xhttp.onreadystatechange = function () {
+         if (this.readyState == 4 && this.status == 200) {
+           displayData(this.responseXML);
+         }
+       };
+       xhttp.open("GET", "people.xml", true);
+       xhttp.send();
+
+       function stringToNode(html) {
+         const template = document.createElement(`template`);
+         html = html.trim(); //removes extra spaces from end of string- clean data
+         template.innerHTML = html;
+         return template.content.firstChild;
+       }
+
+       function createPeople(people) {
+         const content = `<li>
+   <article>
+     <p>${people.first_name + ' '+ people.last_name}</p>
+   </article>
+ </li>`;
+
+         return stringToNode(content);
+       }
+
+       function parsePeople(xml) {
+         const first_name =
+           xml.getElementsByTagName(`first_name`)[0].childNodes[0].nodeValue;
+         const last_name =
+           xml.getElementsByTagName(`last_name`)[0].childNodes[0].nodeValue;
+
+
+         return {
+           first_name,
+           last_name,
+         };
+       }
+
+       function displayData(xmlDoc) {
+         const people = xmlDoc.getElementsByTagName(`person`);
+
+         const list = document.getElementById(`people`);
+
+         for (let index = 0; index < people.length; index++) {
+           const element = people[index];
+           const parsedPeople = parsePeople(element);
+
+           const peopleElement = createPeople(parsedPeople);
+           list.appendChild(peopleElement);
+         }
+       }
