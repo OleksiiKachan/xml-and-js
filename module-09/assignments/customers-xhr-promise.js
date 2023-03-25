4c71695e636c19371acd34f388f00324c3d3a23a
SibiSM <sibichakravarthy888@gmail.com>
2023-03-25 13:32:25 -0400

    Committing the files for Assignment 9

---
+++ module-09/assignments/customers-xhr-promise.js
@@ -0,0 +1,105 @@
+const xhr = (url, method = `GET`) =>
+   new Promise((resolve) => {
+     const xhttp = new XMLHttpRequest();
+     xhttp.onreadystatechange = function () {
+       if (this.readyState == 4 && this.status == 200) {
+         resolve(this.responseXML);
+       }
+     };
+     xhttp.open(method, url);
+     xhttp.send();
+   });
+
+   function stringToNode(html) {
+     const template = document.createElement(`template`);
+     html = html.trim(); 
+     template.innerHTML = html;
+     return template.content.firstChild;
+   }
+
+   function createCustomers(customers) {
+     const content = `
+     <tr>
+     <td>${customers.cust_id}</td>
+     <td>${customers.title} ${customers.name}</td>
+     <td>${customers.address}</td>
+     <td>${customers.phone}</td>
+     <td>${customers.email}</td>
+     <td>
+       <ul>
+         <li>OrderID: ${customers.orderID}</li>
+         <li>OrderBy: ${customers.orderBy}</li>
+         <li>OrderDate: ${customers.orderDate}</li>
+       </ul>
+     </td>
+     <td>
+       <ul>
+         <li>Item price: ${customers.itemPrice}</li>
+        <li>Item quantity: ${customers.itemQty}</li>
+       </ul
+     </td>
+   </tr>
+     `;
+
+     return stringToNode(content);
+   }
+
+   function parseCustomers(xml){
+     const cust_id = xml.attributes[0].nodeValue;
+     const title =
+     xml.getElementsByTagName("name")[0].attributes[0].nodeValue;
+     const name =
+     xml.getElementsByTagName(`name`)[0].childNodes[0].nodeValue;
+     const address =
+     xml.getElementsByTagName(`address`)[0].childNodes[0].nodeValue;
+     const phone =
+     xml.getElementsByTagName(`phone`)[0].childNodes[0].nodeValue;
+
+     if (xml.getElementsByTagName(`email`)[0]) {
+         var email =
+         xml.getElementsByTagName(`email`)[0].childNodes[0].nodeValue;
+     } else {
+         var email = "No email for the given customer";
+     }
+
+     const orderID =
+     xml.getElementsByTagName(`order`)[0].attributes[0].nodeValue;
+     const orderBy =
+     xml.getElementsByTagName(`order`)[0].attributes[1].nodeValue;
+     const orderDate =
+     xml.getElementsByTagName(`orderDate`)[0].childNodes[0].nodeValue;
+     const itemPrice =
+     xml.getElementsByTagName(`itemPrice`)[0].childNodes[0].nodeValue;
+     const itemQty =
+     xml.getElementsByTagName(`itemQty`)[0].childNodes[0].nodeValue;
+
+     return {
+       cust_id,
+       title,
+       name,
+       address,
+       phone,
+       email,
+       orderID,
+       orderBy,
+       orderDate,
+       itemPrice,
+       itemQty,
+     };
+   }
+
+   function displayData(xmlDoc) {
+     const customers_data = xmlDoc.getElementsByTagName(`customer`);
+     const list = document.getElementById(`customer_detail`);
+
+     for (let index = 0; index< customers_data.length; index++) {
+       const cElement = customers_data[index];
+       const parsedCustomers = parseCustomers(cElement);
+
+       const customersElement= createCustomers(parsedCustomers);
+       list.appendChild(customersElement);
+     }
+   }
+
+
+ xhr("customers.xml").then(displayData);
