6cfe85f9ae2cad3d49b52a3508d29b31c1b8b0a6
rhornedo <115593654+rhornedo@users.noreply.github.com>
2023-04-05 13:26:01 -0400

    complete activity 1

--- module-10/assignments/activity.md
+++ module-10/assignments/activity.md
@@ -3,3 +3,7 @@
 - Create Spotify dev account​
 - Create new app​
 - Using your client id and client secret create html page to display genres and playlists by genres (don't copy my page design)
+
+
+
+![image activity1](./activity1.png)
---
+++ module-10/assignments/app.js
@@ -0,0 +1,79 @@
+const clientId = `c1c0c189d0f5491c8857eed6431af4d1`;
+const clientSecret = `167b0e3f3587465c85d49012408539f7`;
+
+const getToken = async () => {
+    const result = await fetch("https://accounts.spotify.com/api/token", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            Authorization: "Basic " + btoa(clientId + ":" + clientSecret),
+        },
+        body: "grant_type=client_credentials",
+    });
+
+    const data = await result.json();
+    return data.access_token;
+};
+
+const getGenres = async (token) => {
+    const result = await fetch(
+        `https://api.spotify.com/v1/browse/categories?locale=sv_US`,
+        {
+            method: "GET",
+            headers: { Authorization: "Bearer " + token },
+        }
+    );
+
+    const data = await result.json();
+    return data.categories.items;
+};
+
+const getPlaylistByGenre = async (token, genreId) => {
+    const limit = 10;
+  
+    const result = await fetch(
+      `https://api.spotify.com/v1/browse/categories/${genreId}/playlists?limit=${limit}`,
+      {
+        method: "GET",
+        headers: { Authorization: "Bearer " + token },
+      }
+    );
+  
+    const data = await result.json();
+    return data.playlists.items;
+  };
+  
+  const load = async () => {
+    const token = await getToken();
+    const genres = await getGenres(token);
+    const list = document.getElementById(`genres`);
+    genres.map(async ({ name, id, icons: [icon], href }) => {
+      const playlists = await getPlaylistByGenre(token, id);
+      const playlistsList = playlists
+        .map(
+          ({ name, external_urls: { spotify }, images: [image] }) => `
+          <li>
+            <a href="${spotify}" alt="${name}" target="_blank">
+              <img src="${image.url}" width="180" height="180"/>
+            </a>
+          </li>`
+        )
+        .join(``);
+  
+        const html = `
+        <article class="genre-card">
+          <img src="${icon.url}" width="${icon.width}" height="${icon.height}" alt="${name}"/>
+          <div>
+            <h2>${name}</h2>
+            <ol>
+              ${playlistsList}
+            </ol>
+          </div>
+        </article>`;
+  
+        list.insertAdjacentHTML("beforeend", html);
+      
+    });
+  };
+  
+  load();
