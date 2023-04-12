f16c3d46bde1ef2d91eed295cd948a5b474e2515
Achewie95 <113118255+Achewie95@users.noreply.github.com>
2023-04-12 17:36:08 -0400

    completed activity

--- module-11/assignments/client/text.js
+++ module-11/assignments/client/text.js
@@ -1,16 +1,16 @@
-const clientId = "1cd4b8919a804e35af673e40b29986e6";
+const clientID = "1cd4b8919a804e35af673e40b29986e6";
 const clientSecret = "a5bea6c96ef945ad813e33fb59919bb7";
 
 let _data = [];
 
 const getToken = async () => {
-  const result = await fetch("https://accounts.spotify.com/api/token", {
-    method: "POST",
+  const result = await fetch(`https://accounts.spotify.com/api/token`, {
+    method: `POST`,
     headers: {
       "Content-Type": "application/x-www-form-urlencoded",
-      Authorization: "Basic " + btoa(clientId + ":" + clientSecret),
+      Authorization: `Basic ${btoa(`${clientID}:${clientSecret}`)}`,
     },
-    body: "grant_type=client_credentials",
+    body: `grant_type=client_credentials`,
   });
 
   const data = await result.json();
@@ -18,31 +18,41 @@ const getToken = async () => {
 };
 
 const getGenres = async (token) => {
-  const result = await fetch(
-    `https://api.spotify.com/v1/browse/categories?locale=sv_US`,
-    {
-      method: "GET",
-      headers: { Authorization: "Bearer " + token },
-    }
-  );
-
+  const result = await fetch(`https://api.spotify.com/v1/browse/categories`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
   const data = await result.json();
   return data.categories.items;
 };
 
-const getPlaylistByGenre = async (token, genreId) => {
+const getPlaylistsByGenre = async (token, genreId) => {
   const limit = 10;
-
   const result = await fetch(
     `https://api.spotify.com/v1/browse/categories/${genreId}/playlists?limit=${limit}`,
     {
       method: "GET",
-      headers: { Authorization: "Bearer " + token },
+      headers: { Authorization: `Bearer ${token}` },
     }
   );
-
   const data = await result.json();
   return data.playlists ? data.playlists.items : [];
+};
+
+const getTracksByPlaylist = async (token, playlistId) => {
+  const result = await fetch(
+    `https://api.spotify.com/v1/playlists/${playlistId}/tracks`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+  const data = await result.json();
+  return data.items ? data.items.slice(0, 5) : [];
 };
 
 const loadGenres = async () => {
@@ -51,9 +61,14 @@ const loadGenres = async () => {
 
   _data = await Promise.all(
     genres.map(async (genre) => {
-      const playlists = await getPlaylistByGenre(token, genre.id);
-
-      return { ...genre, playlists };
+      const playlists = await getPlaylistsByGenre(token, genre.id);
+      const tracks = await Promise.all(
+        playlists.map(async (playlist) => {
+          const items = await getTracksByPlaylist(token, playlist.id);
+          return { ...playlist, tracks: items };
+        })
+      );
+      return { ...genre, playlists: tracks };
     })
   );
 };
@@ -62,10 +77,8 @@ const renderGenres = (filterTerm) => {
   let source = _data;
 
   if (filterTerm) {
-    console.log(filterTerm);
     const term = filterTerm.toLowerCase();
     source = source.filter(({ name }) => {
-      console.log(name.toLowerCase().includes(term));
       return name.toLowerCase().includes(term);
     });
   }
@@ -75,14 +88,23 @@ const renderGenres = (filterTerm) => {
   const html = source.reduce((acc, { name, icons: [icon], playlists }) => {
     const playlistsList = playlists
       .map(
-        ({ name, external_urls: { spotify }, images: [image] }) => `
+        ({ name, external_urls: { spotify }, images: [image], tracks }) => `
         <li>
           <a href="${spotify}" alt="${name}" target="_blank">
             <img src="${image.url}" width="180" height="180"/>
           </a>
+          ${tracks
+            .map(
+              ({ track: { name: trackName, artists } }) =>
+                `<p>
+            <strong>${trackName}</strong> <br>
+            Artist: ${artists.map((artist) => artist.name).join(``)}
+          </p>`
+            )
+            .join(``)}
         </li>`
       )
-      .join(``);
+      .join("");
 
     if (playlists) {
       return (
