f4885edb2723198e2a457d01b6e631d1ba721a04
nishasonii17 <nishasoni0011@gmail.com>
2023-04-15 05:20:09 -0400

    assignment-11

--- module-11/assignments/activity.js
+++ module-11/assignments/activity.js
@@ -1,5 +1,5 @@
 const clientId = `081b1851ceb440ffbdd9397cf729c311`;
-const clientSecret = `7e2f31fde9a40a1aec3d284ae2401f5`;
+const clientSecret = `57e2f31fde9a40a1aec3d284ae2401f5`;
 
 const getToken = async () => {
     const result = await fetch("https://accounts.spotify.com/api/token", {
@@ -10,11 +10,11 @@ const getToken = async () => {
       },
       body: "grant_type=client_credentials",
     });
-  
+
     const data = await result.json();
     return data.access_token;
   };
-  
+
   const getGenres = async (token) => {
     const result = await fetch(
       `https://api.spotify.com/v1/browse/categories?locale=sv_US`,
@@ -23,129 +23,114 @@ const getToken = async () => {
         headers: { Authorization: "Bearer " + token },
       }
     );
-  
+
     const data = await result.json();
     return data.categories.items;
   };
-  
-  const getPlaylistByGenre = async (token, genreId) => {
-    const limit = 10;
-  
+  const getPlaylistsByGenre = async(token,genreId)=>{
+    const limit = 6;
     const result = await fetch(
       `https://api.spotify.com/v1/browse/categories/${genreId}/playlists?limit=${limit}`,
       {
         method: "GET",
-        headers: { Authorization: "Bearer " + token },
+        headers: { Authorization: "Bearer " + token }
       }
     );
-  
+
     const data = await result.json();
     return data.playlists.items;
+
   };
-  
-  
-  const getPlaylistByTracks = async (token, playlistId) => {
-    const limit = 3;
+const getPlaylistTracks= async (token, playlistId) => {
+    const limit = 5;
     const result = await fetch(
       `https://api.spotify.com/v1/playlists/${playlistId}/tracks?limit=${limit}`,
       {
         method: "GET",
-        headers: {
-          Authorization: "Bearer " + token,
-        },
-      }
+        headers: { 
+          Authorization: "Bearer " + token },
+      },
     );
-  
+
     const data = await result.json();
-    return data.items || [];
+    return data;
   };
-  const loadGenres = async () => {
+
+  const loadGenres = async()=>{
     const token = await getToken();
     const genres = await getGenres(token);
-  
+
     _data = await Promise.all(
       genres.map(async (genre) => {
-        const playlists = await getPlaylistByGenre(token, genre.id);
-        const playlistsList = await Promise.all(
-          playlists.map(async (playlist) => {
-            const tracks = await getPlaylistByTracks(token, playlist.id);
-              return { ...playlist, tracks };
-          })
-        );
-        return { ...genre, playlists: playlistsList };
+        const playlists = await getPlaylistsByGenre(token, genre.id);
+        if(playlists.length){
+            await Promise.all(playlists.map(async (playlist)=>{
+              playlist.tracks = await getPlaylistTracks(token, playlist.id);
+            })
+          );
+        }
+
+        return { ...genre, playlists };
       })
     );
   };
-  
-  
-  const renderGenres = async (filterTerm) => {
-    const token = await getToken();
-    const genres = await getGenres(token);
+
+  let _playlist = 'true';
+
+  const renderGenres = (filterTerm) => {
     let source = _data;
-  
+
     if (filterTerm) {
       const term = filterTerm.toLowerCase();
       source = source.filter(({ name }) => {
         return name.toLowerCase().includes(term);
       });
     }
+
+
     const list = document.getElementById(`genres`);
-    const html = source.reduce((acc, { name, icons: [icon], playlists }) => {
-      const playlistsList = playlists
-        .map(({  name, id, images: [image], external_urls: { spotify } , tracks }) => {
-  
-          const listItems = tracks
-                .map(
-                  ({ track: { name: trackName, artists } }) =>
-                    `<div class="all-name">
-                  <b class="track-name">${trackName}</b> </br>
-                    <div class="artist-name">${artists
-                      .map((artist) => artist.name)
-                      .join("")} </div>
-                </div>`
-                )
-                .join("");
-  
-          return `<li class="main-list">
-          <a href="${spotify}" alt="${name}" target="_blank">
-              <img src="${image.url}" width="150" height="150"/>
-          </a>
-          <div class="play-name" > 
-              <a href="${spotify}" alt="${name}" target="_blank">${name}</a>
-          </div>
-            ${listItems}
-          </li>`;
-        }
-        );
-  
-      if (playlists) {
-        return (
-          acc +
-          `
-          <article class="card">
-              <img class="main-img" src="${icon.url}" width="${icon.width}" height="${
-        icon.height
-      }" alt="${name}"/>
-            <div>
-               <h2>${name}</h2>
-               <ol class="list">
-                ${playlistsList}
+    const html = source.reduce((acc,{id,name,href,icons:[icon],playlists})=>{
+     return acc + `
+     <article class="genre-card">      
+     <img src="${icon.url}" width="${icon.width}" height="${icon.height}" class="rounded-img" alt="${name}"/>
+     <div>
+     <h2>${name}</h2>
+     ${_playlist != undefined && _playlist ==='true' ? 
+       `<ul>
+         ${playlists.map(({name, images:[image], external_urls:{spotify}, id,tracks}) => {
+           return `
+             <li>
+               <a href="${spotify}" target="_blank">
+                 <img src="${image.url}" width="180" height="180" class="rounded-img" alt="${name}"/>
+               </a>
+               <ol>
+                 ${tracks.items.map(({track:{name, external_urls:{spotify}}}) => `
+                   <li class="track-name"><a href="${spotify}" target="_blank">${name}</a></li>
+                 `).join('')}
                </ol>
-            </div>
-          </article>`
-         );
-      }
-    }, ``);
-  
+             </li>
+           `;
+         })}
+       </ul>` : 
+       ''
+     }
+     </div>
+   </article>
+     `
+
+    },'');
+
     list.innerHTML = html;
-  };
-  
+  }
+
+
+
   loadGenres().then(renderGenres);
-  
+
   const onSubmit = (event) => {
     event.preventDefault();
-  
+
     const term = event.target.term.value;
-  
+     _playlist = event.target._radio.value;
     renderGenres(term);
   };
